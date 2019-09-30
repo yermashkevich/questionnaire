2ec7b9ce2a9c2b7bf20d3e98a32df7ad435e7daf
ermashkevich <olyabelka1993@gmail.com>
2019-10-01 00:05:38 +0300

    result

--- js/index.js
+++ js/index.js
@@ -4,6 +4,15 @@ $(function() {
         $(this).toggleClass("active");
     });
 
+    $('.menu__list-link').on("click", function(e){
+        let el = $(this).attr("href");
+
+        $('.menu__list-link').removeClass("active");
+        $(this).addClass("active");
+        $('html,body').stop().animate({ scrollTop: $(el).offset().top - 15 }, 800);
+        e.preventDefault();
+    });
+
     $(".section__input").on("focus", function() {
         $(this).prev($(".section__label")).addClass("focus");
     });
@@ -13,6 +22,10 @@ $(function() {
             $(this).prev($(".section__label")).removeClass("focus");
         }
     });
+
+    if($(".section__input").val().length > 0) {
+        $(".section__input").prev($(".section__label")).addClass("focus");
+    } 
 
     $(".js-select").on("focus", function() {
         $(".js-list").show();
@@ -25,6 +38,16 @@ $(function() {
     $(".person-fields__select-item").on("mousedown", function(){
         let value = $(this).attr("data-value");
         $(".person-fields__select").attr("value", value);
+    });
+
+    let rangeLevel = $(".level-fields__input[checked]").attr("data-value");
+    $(".level-fields__range-check").width(rangeLevel);
+    $(".level-fields__range-icon").css("left", rangeLevel);
+
+    $(".level-fields__label").on("click", function() {
+        let rangeLevel = $(this).children(".level-fields__input").attr("data-value");
+        $(".level-fields__range-check").width(rangeLevel);
+        $(".level-fields__range-icon").css("left", rangeLevel);
     });
 
 });
